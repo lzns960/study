b1ed8f18fa7082e27ad5d98b0ef75247fce282f9
Suji Park <lzns960@gmail.com>
2022-09-27 17:55:01 +0900

    feat: react part2. 쇼피몽프로젝트

---
+++ coding_apple/react/shop/src/data.js
@@ -0,0 +1,24 @@
+let data = [
+  {
+    id: 0,
+    title: "말티즈",
+    content: "말티즈는 참지않긔",
+    price: 5000
+  },
+
+  {
+    id : 1,
+    title : "포메",
+    content : "날아다니는 솜뭉치",
+    price : 110000
+  },
+
+  {
+    id : 2,
+    title : "비숑",
+    content : "솜사탕 한입 하실라우?",
+    price : 130000
+  }
+]
+
+export default data;
