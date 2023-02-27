20748ac0d144e62ca422de10e4b540140fec52a5
lzns960 <lzns960@gmail.com>
2023-02-27 16:40:46 +0900

    uniontype 연습문제

--- coding_apple/typescript/index.ts
+++ coding_apple/typescript/index.ts
@@ -17,3 +17,20 @@ let project :{ member: string[], days: number, started: boolean } = {
   started: true,
 }
 
+// TS part1 4 숙제
+let user :string = "kim";
+let age :undefined | number = undefined;
+let married :boolean = false;
+let 철수 :(string|boolean|undefined|number)[]= [user, age, married];
+
+let 학교: { 
+  score: (number|boolean)[],
+  teacher: string,
+  friend: string| string[]
+}  =  {
+  score  : [100, 97, 84],
+  teacher : 'Phil',
+  friend: 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher]
